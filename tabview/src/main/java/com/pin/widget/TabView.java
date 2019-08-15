68ff593f2a6468820274b9cc030b3e786fd8a8f9
taochen <tao_chen@pinssible.com>
2019-08-15 14:33:33 +0800

    [feature] update package name

--- tabview/src/main/java/com/pin/widget/TabView.java
+++ tabview/src/main/java/com/pin/widget/TabView.java
@@ -1,4 +1,4 @@
-package com.pin.tabview;
+package com.pin.widget;
 
 import android.content.Context;
 import android.content.res.TypedArray;
@@ -8,6 +8,8 @@ import android.util.AttributeSet;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.pin.tabview.R;
 
 
 public class TabView extends FrameLayout {
